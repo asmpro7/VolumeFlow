4579a6671527856afb9b64371f35e36e520d7189
AminSallah <124622454+AminSallah@users.noreply.github.com>
2023-10-09 23:50:38 +0300

    Options appear based on user input.

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
+actions = {
+    "Volume up":["Images/up.png", {"method": "up", "parameters": []}],
+    
+    "Volume down":["Images/down.png", {"method": "down", "parameters": []}],
+    
+    "Maximum Volume": ["Images/max.png", {"method": "max", "parameters": []}],
+    
+    "mute":  ["Images/mute.png", {"method": "mute", "parameters": []}],
+    
+    "unmute": ["Images/unmute.png", {"method": "unmute", "parameters": []}],
+    
+}
 
 from flowlauncher import FlowLauncher
 import subprocess
@@ -15,46 +27,29 @@ class VolumeFlow(FlowLauncher):
 
     def query(self, query):
         results=[]
-        if query == "up":
-            subprocess.run(f"nircmd.exe changesysvolume 10000")
-        elif query == "down":
-            subprocess.run(f"nircmd.exe changesysvolume -10000")
-        elif query == "max":
-            subprocess.run(f"nircmd.exe changesysvolume 65535")
-        elif query == "mute":
-            subprocess.run(f"nircmd.exe mutesysvolume 1")
-        elif query == "unmute":
-            subprocess.run(f"nircmd.exe mutesysvolume 0")
-        elif query.isdigit():
-            value=65535*int(query)/100
-            subprocess.run(f"nircmd.exe changesysvolume -65535")
-            subprocess.run(f"nircmd.exe changesysvolume {value}")
-        else:
-            results.append({
-                    "Title": "Volume up",
-                    "IcoPath": f"Images/up.png",
-                    "JsonRPCAction": {"method": "up", "parameters":[]  }
-                    })
-            results.append({
-                    "Title": "Volume down",
-                    "IcoPath": f"Images/down.png",
-                    "JsonRPCAction": {"method": "down", "parameters":[]  }
-                    })
-            results.append({
-                    "Title": "Maximum Volume",
-                    "IcoPath": f"Images/max.png",
-                    "JsonRPCAction": {"method": "max", "parameters":[]  }
-                    })
-            results.append({
-                    "Title": "mute",
-                    "IcoPath": f"Images/mute.png",
-                    "JsonRPCAction": {"method": "mute", "parameters":[]  }
-                    })
-            results.append({
-                    "Title": "unmute",
-                    "IcoPath": f"Images/unmute.png",
-                    "JsonRPCAction": {"method": "unmute", "parameters":[]  }
-                    })
+        # if query == "up":
+        #     subprocess.run(f"nircmd.exe changesysvolume 10000")
+        # elif query == "down":
+        #     subprocess.run(f"nircmd.exe changesysvolume -10000")
+        # elif query == "max":
+        #     subprocess.run(f"nircmd.exe changesysvolume 65535")
+        # elif query == "mute":
+        #     subprocess.run(f"nircmd.exe mutesysvolume 1")
+        # elif query == "unmute":
+        #     subprocess.run(f"nircmd.exe mutesysvolume 0")
+        # elif query.isdigit():
+        #     value=65535*int(query)/100
+        #     subprocess.run(f"nircmd.exe changesysvolume -65535")
+        #     subprocess.run(f"nircmd.exe changesysvolume {value}")
+        if True:
+            user_input = [item for item in actions.keys() if query.strip().lower() in item.strip().lower()]
+            for action in user_input:
+                results.append({
+                    "title": action,
+                    "IcoPath": actions[action][0],
+                    "JsonRPCAction": actions[action][1]
+                })
+                
             
 
 
